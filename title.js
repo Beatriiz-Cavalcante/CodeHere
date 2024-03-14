021fd49f879aea19ca74b6e915e550c8113519eb
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-03-13 23:13:26 -0300

    Estilização tipo app

---
+++ title.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ['Here:)'],
+        typeSpeed: 120, //especificação da valecidade de digitação
+        loop: false  // definição se o efeito se repete ou não
+    };
+
+    var typed = new Typed('#here', options);
+});
